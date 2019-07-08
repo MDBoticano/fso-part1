b0c529cf2c92e3a15480ce2d8cb8992839959b37
mdboticano <mdboticano@gmail.com>
2019-07-08 16:55:29 -0700

    Exercise 1.11*: unicafe step6 (HTML table)

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({clickHandler, text}) => {
-  return( 
+const Button = ({ clickHandler, text }) => {
+  return (
     <button onClick={clickHandler}>{text}</button>
   )
 }
 
-const Statistics = ({stats}) => {
+const Statistics = ({ stats }) => {
   const allStats = stats.good + stats.neutral + stats.bad;
-  const avgStats = (stats.good + 0*stats.neutral + -1*stats.bad) / allStats;
-  const positiveStats = (stats.good/allStats * 100 + ' %');
+  const avgStats = (stats.good + 0 * stats.neutral + -1 * stats.bad) / allStats;
+  const positiveStats = (stats.good / allStats * 100 + ' %');
 
   const statsList = [
     { text: 'good', value: stats.good },
@@ -20,23 +20,29 @@ const Statistics = ({stats}) => {
     { text: 'average', value: avgStats },
     { text: 'positive', value: positiveStats }
   ]
-  
-  if (allStats === 0 ) {
-    return ( <p>No feedback given</p> )
-  } 
 
-  return(
-    <div>
-      {statsList.map((obj, index) => 
-        <Statistic value={obj.value}  text={obj.text} key={index}/>
-      )}
-    </div>
+  if (allStats === 0) {
+    return (<p>No feedback given</p>)
+  }
+
+  return (
+    <table>
+      <tbody>
+        {statsList.map((obj, index) =>
+          <Statistic value={obj.value} text={obj.text} key={index} />
+        )}
+      </tbody>
+    </table>
   )
 }
 
-const Statistic = ({text, value}) => {
+const Statistic = ({ text, value }) => {
   return (
-    <p>{text} {value}</p>
+    // <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -49,11 +55,11 @@ const App = (props) => {
   return (
     <div>
       <h2>Give Feedback</h2>
-      <Button clickHandler={()=> setGood(good + 1)} text='good' />
-      <Button clickHandler={()=> setNeutral(neutral + 1)} text='neutral' />
-      <Button clickHandler={()=> setBad(bad + 1)} text='bad' />
+      <Button clickHandler={() => setGood(good + 1)} text='good' />
+      <Button clickHandler={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button clickHandler={() => setBad(bad + 1)} text='bad' />
       <h2>statistics</h2>
-      <Statistics stats={{good, neutral, bad}} />
+      <Statistics stats={{ good, neutral, bad }} />
     </div>
   )
 }
